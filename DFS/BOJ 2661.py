02a555f22b8d52e85844eff58a216fbe9efb3f2a
acupoframen <choe.hwan@gmail.com>
2022-12-24 09:35:58 +0100

    Create BOJ 2661.py

---
+++ DFS/BOJ 2661.py
@@ -0,0 +1,18 @@
+def goodCheck(arr):
+    for i in range(1,len(arr)//2+1):
+       if arr[len(arr)-i:len(arr)]==arr[len(arr)-2*i:len(arr)-i]:
+           return False
+    return True
+def dfs(arr,idx):
+    if idx==n:
+        print(''.join(str(i) for i in arr))
+        exit()
+    for i in range(1,4):
+        if goodCheck(arr+[i]):
+            arr.append(i)
+            dfs(arr,idx+1)
+            arr.pop()
+    
+            
+n=int(input())
+dfs([],0)
