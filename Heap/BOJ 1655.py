64120c36c4e01bde555cc88e163950dbcf56ed24
acupoframen <choe.hwan@gmail.com>
2023-01-05 15:23:03 +0100

    Create BOJ 1655.py

---
+++ Heap/BOJ 1655.py
@@ -0,0 +1,18 @@
+import heapq
+import sys
+input=sys.stdin.readline
+maxHeap=[]
+minHeap=[]
+n=int(input())
+for i in range(n):
+    num=int(input())
+    if len(maxHeap)==len(minHeap):
+        heapq.heappush(minHeap, -1*num)
+    else:
+        heapq.heappush(maxHeap, num)
+    if maxHeap and -minHeap[0]>maxHeap[0]:
+        a=heapq.heappop(minHeap,minHeap[0])
+        b=heapq.heappop(maxHeap,maxHeap[0])
+        heapq.heappush(minHeap,-b)
+        heapq.heappush(maxHeap,-a)
+    print(-minHeap[0])
