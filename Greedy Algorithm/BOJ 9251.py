973071b222ed03677ad678fd5f3cc15d93355997
acupoframen <choe.hwan@gmail.com>
2023-01-12 08:44:57 +0900

    Create BOJ 9251.py

---
+++ Greedy Algorithm/BOJ 9251.py
@@ -0,0 +1,15 @@
+a=input()
+b=input()
+lena=len(a)
+lenb=len(b)
+i=0
+dp=[[0 for _ in range(lenb+1)] for _ in range(lena+1)]
+print(dp)
+for i in range(lena):
+    for j in range(lenb):
+        if a[i]==b[j]:
+            dp[i+1][j+1]=dp[i][j]+1
+        else: 
+            dp[i+1][j+1]=max(dp[i][j+1],dp[i+1][j])
+print(dp[lena][lenb])
+print(dp)
