de7deead8cf0136ca62268c30a832d9e5fb56354
acupoframen <choe.hwan@gmail.com>
2023-03-13 23:27:39 +0900

    Create BOJ 1002.py

---
+++ Geometry/BOJ 1002.py
@@ -0,0 +1,16 @@
+t=int(input())
+for _ in range(t):
+    data=list(map(int,input().split()))
+    d=((data[0]-data[3])**2+(data[1]-data[4])**2)**0.5
+    if not d:
+        if data[2]==data[5]:
+            print(-1)
+        else:
+            print(0)
+    else:
+        if d==data[2]+data[5] or d==abs(data[2]-data[5]):
+            print(1)
+        elif data[2]+data[5]>d>abs(data[2]-data[5]):
+            print(2)
+        else:
+            print(0)
