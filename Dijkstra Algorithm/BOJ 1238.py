46187e022cc6d6a570c3b476f461bddc926fb9f8
acupoframen <choe.hwan@gmail.com>
2023-03-16 22:46:24 +0900

    Create BOJ 1238.py

---
+++ Dijkstra Algorithm/BOJ 1238.py
@@ -0,0 +1,30 @@
+import sys
+import heapq
+input=sys.stdin.readline
+n,m,x=map(int,input().split())
+d_x=[1e10 for _ in range(n+1)]
+roadData=[[] for _ in range(n+1)]
+for _ in range(m):
+    a,b,c=map(int,input().split())
+    roadData[a].append([b,c])
+def dijkstra(num):
+    d=[1e10 for _ in range(n+1)]
+    d[num]=0
+    q=[]
+    heapq.heappush(q,[0,num])
+    while q:
+        dist,oldCoord=heapq.heappop(q)
+        if dist>d[oldCoord]:
+            continue
+        for where,value in roadData[oldCoord]:
+            if d[where]>value+d[oldCoord]:
+                d[where]=value+d[oldCoord]
+                heapq.heappush(q,[d[where],where])
+    return d
+answer=0
+d_x=dijkstra(x)
+print(d_x)
+for i in range(1,n+1):
+    print(dijkstra(i))
+    answer=max(answer,dijkstra(i)[x]+d_x[i])
+print(answer)
