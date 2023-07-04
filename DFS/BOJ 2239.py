0a5379db437e09f3b6156417acad6f471794460c
acupoframen <choe.hwan@gmail.com>
2023-07-04 11:53:25 +0900

    Create BOJ 2239.py

---
+++ DFS/BOJ 2239.py
@@ -0,0 +1,36 @@
+import sys
+input=sys.stdin.readline
+data,blank= [],[]
+for i in range(9):
+    data.append(list(map(int,list(input().strip()))))
+    for j in range(9):
+        if not data[i][j]:
+            blank.append((i,j))
+
+def check(x,y):
+    existingNum=set()
+    for i in range(9):
+        existingNum.add(data[x][i])
+        existingNum.add(data[i][y])
+    xrange=[num for num in range((x//3)*3,(x//3)*3+3)]
+    yrange=[num for num in range((y//3)*3,(y//3)*3+3)]
+    for a in xrange:
+        for b in yrange:
+            existingNum.add(data[a][b])
+    numbers=[num for num in range(1,10) if num not in list(existingNum)]
+    return numbers
+
+    
+def dfs(idx):
+    if len(blank)==idx:
+        for i in range(9):
+            print(*data[i],sep='')
+        exit(0)
+    x,y=blank[idx]
+    numbers=check(x,y)
+    for num in numbers:
+        data[x][y]=num
+        dfs(idx+1)
+        data[x][y]=0
+
+dfs(0)
