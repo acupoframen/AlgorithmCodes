f67f498cccdcfdf91f447c8e928ce90feb77762c
acupoframen <choe.hwan@gmail.com>
2023-09-08 14:23:01 +0900

    BOJ 2023_09_08

--- D.py
+++
@@ -1,5 +0,0 @@
-p=int(input())
-for _ in range(p):
-    n,m=map(int,input().split())
-
-data=
---
+++ Sweeping/BOJ 2836.py
@@ -0,0 +1,24 @@
+data=[]
+n,m=map(int,input().split())
+for _ in range(n):
+    start,end=map(int,input().split())
+    if start>end:
+        data.append([end,start])
+
+data.sort(key= lambda x: -x[1])
+temp=[]
+tempstart, tempend= data[0]
+for i in range(1,len(data)):
+
+    start, end=data[i]
+
+    if tempstart<=end:
+        tempstart=min(tempstart,start)
+    else:
+        temp.append([tempstart,tempend])
+        tempstart,tempend=start,end
+temp.append([tempstart,tempend])
+answer=m
+for i in range(len(temp)):
+    answer+= 2*(temp[i][1]-temp[i][0])
+print(answer)
