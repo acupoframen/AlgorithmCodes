53c5e91c1f834d8d02a7e4d741fafbcd412dd8bc
acupoframen <choe.hwan@gmail.com>
2023-10-04 21:21:58 +0900

    BOJ 2023_10_04

---
+++ Prefix Sum/BOJ 11659.py
@@ -0,0 +1,12 @@
+n,m=map(int,input().split())
+data=list(map(int,input().split()))
+sumdata=[0]
+temp=0
+for i in range(n):
+    temp+=data[i]
+    sumdata.append(temp)
+
+for _ in range(m):
+    a,b=map(int,input().split())
+    
+    print(sumdata[b]-sumdata[a-1])
