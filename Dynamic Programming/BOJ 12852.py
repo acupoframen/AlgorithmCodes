e972a0af90785b626beb2764f8b22b19ecbfcdf7
acupoframen <choe.hwan@gmail.com>
2023-10-11 08:52:19 +0900

    BOJ 2023_10_11

---
+++ Dynamic Programming/BOJ 12852.py
@@ -0,0 +1,27 @@
+from collections import deque
+n=int(input())
+data=[0 for _ in range(n+1)]
+q=deque([1])
+while q:
+    k=q.popleft()
+    if n==k+1 or n==2*k or n==3*k:
+        data[n]=k
+        break
+    if 3*k<n and not data[3*k]:
+        data[3*k]=k
+        q.append(3*k)
+    if 2*k<n and not data[2*k]:
+        data[2*k]=k
+        q.append(2*k)
+    if not data[k+1]:
+        q.append(k+1)
+        data[k+1]=k
+
+answer=[n]
+val=n
+while val!=1:
+    val=data[val]
+    answer.append(val)
+
+print(len(answer)-1)
+print(*answer)
