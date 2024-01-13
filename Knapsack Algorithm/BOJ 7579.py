f6207e0a97578bbd537245601f41890f9852cbdf
acupoframen <choe.hwan@gmail.com>
2024-01-13 12:41:06 +0900

    BOJ 2024_01_13

--- Knapsack Algorithm/BOJ 7579.py
+++ Knapsack Algorithm/BOJ 7579.py
@@ -1,13 +1,9 @@
 n,m=map(int,input().split())
 data=list(map(int,input().split()))
 cost=list(map(int,input().split()))
-
 answer=1e10
-
 total=sum(cost)
-
 dp=[[0 for _ in range(total+1)] for _ in range(n+1)]
-
 for i in range(n):
     for j in range(total):
         if cost[i]>j:
@@ -16,11 +12,10 @@ for i in range(n):
             dp[i][j]=max(dp[i-1][j],data[i]+dp[i-1][j-cost[i]])
         if dp[i][j]>=m:
             answer=min(answer,j)
-if m==0:
+if sum(cost)==0:
     print(0)
-elif n==1:
-    print(cost[0])
-elif answer==1e10:
-    print(n*m)
+    exit(0)
+if answer==1e10:
+    print(total)
 else:
     print(answer)
