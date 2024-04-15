5e10b566df34efb91b3b0226eeb5ef106aaa9fff
acupoframen <choe.hwan@gmail.com>
2024-04-15 14:42:31 +0900

    BOJ 2024_04_15

---
+++ Binary Search/BOJ 1477.py
@@ -0,0 +1,23 @@
+n,m,l=map(int,input().split())
+
+data=list(map(int,input().split()))
+data.append(0)
+data.append(l)
+data.sort()
+answer=0
+left=1
+right=l-1
+dist=[]
+for i in range(1,len(data)):
+    dist.append(data[i]-data[i-1])
+while left<=right:
+    mid=(left+right)//2 #거리
+    temp=0 #주유소 설치 가능 개수
+    for i in dist:
+        temp+=(i-1)//mid
+    if temp<=m: #주유소 적음. 덜 설치 가능.
+        right=mid-1
+        answer=mid
+    else: #주유소 너무 많이 필요함. 거리를 줄여야함.
+        left=mid+1
+print(answer)
