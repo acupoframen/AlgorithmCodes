52b0763002455f2743d2234476a03380e6ac4333
acupoframen <choe.hwan@gmail.com>
2024-06-24 09:39:02 +0900

    BOJ 2024_06_24

---
+++ String/Palindrome/BOJ 14444.py
@@ -0,0 +1,17 @@
+s=input()
+s="#"+"#".join(s)+"#"
+n=len(s)
+
+def manacher():
+    a=list(0 for _ in range(n))
+    r,p=0,0
+    for i in range(n):
+        if i<=r:
+            a[i]=min(a[2*p-i],r-i)
+        while i-a[i]-1>=0 and i+a[i]+1<n and s[i-a[i]-1]==s[i+a[i]+1]:
+            a[i]+=1
+        if r<a[i]+i:
+            r=a[i]+i
+            p=i
+    return a
+print(max(manacher()))
