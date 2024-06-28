89c457778781303289cfaa80262b40f675f78a0a
acupoframen <choe.hwan@gmail.com>
2024-06-28 11:02:56 +0900

    BOJ 2024_06_28

--- Brute Force/BOJ 1366.py
+++ Brute Force/BOJ 1366.py
@@ -1,7 +1,28 @@
-tune=['A','A#','B','C','C#','D','D#','E','F','F#','G','G#']
+from itertools import product
 n,m=map(int,input().split())
-data=list(input().split())
+tones=list(("A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"))
+current=list(input().split())
 code=list(input().split())
 answer=1e10
-for i in range(data):
-    for j in code:
+for goal in product(range(m),repeat=n):
+    if len(set(goal))!=m:
+        continue
+    flats=[]
+    for k in range(n):
+        number=0
+        currenttone=tones.index(current[k])
+        if tones[currenttone]==code[goal[k]]:
+            continue
+        while tones[(currenttone+number)%12]!=code[goal[k]]:
+            number+=1
+        flats.append(number)
+    if flats:
+        temp=[]
+        temp.append(max(flats)-min(flats)+1)
+        flats.sort()
+        for i in range(1, len(flats)):
+            temp.append(1+flats[i-1]+12-flats[i])
+        answer=min(answer,min(temp))
+    else:
+        answer=0
+print(answer)
