aeff4866c6d12487024e053ec9e511534d7c8bef
acupoframen <choe.hwan@gmail.com>
2024-07-19 06:56:47 +0100

    BOJ 2024_07_19

--- implementation/BOJ 2083.py
+++ implementation/BOJ 2083.py
@@ -1,1 +1,10 @@
-xx
+while True:
+    a,b,c=input().split()
+    b=int(b)
+    c=int(c)
+    if a=='#' and b==0 and c==0:
+        break
+    if b<=17 and c<80:
+        print(a,"Junior")
+    else:
+        print(a,"Senior")
