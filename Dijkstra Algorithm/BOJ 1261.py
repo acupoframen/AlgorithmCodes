43a5c42046a969e8dae85813db4b9e3a485ce5a6
acupoframen <choe.hwan@gmail.com>
2024-09-12 14:34:10 +0900

    BOJ 2024_09_12

--- Dijkstra Algorithm/BOJ 1261.py
+++ Dijkstra Algorithm/BOJ 1261.py
@@ -1,27 +1,28 @@
 import heapq
+import sys
+input=sys.stdin.readline
 n,m=map(int,input().split())
-data=list(list(map(int,list(input()))) for _ in range(m))
+data=list(list(map(int,input().strip())) for _ in range(m))
 q=[]
-q.append([0,0,0])
-currdist=list(list(1e10 for _ in range(n)) for _ in range(m))
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
+visited=list(list(1e10 for _ in range(n)) for _ in range(m))
+heapq.heappush(q,[0,0,0])
+visited[0][0]=0
+dx=[-1,0,0,1]
+dy=[0,-1,1,0]
 while q:
     d,x,y=heapq.heappop(q)
-    if currdist[x][y]<d:
-        continue
     if x==m-1 and y==n-1:
         print(d)
         break
+    if visited[x][y]<d:
+        continue
     for k in range(4):
         nx=x+dx[k]
         ny=y+dy[k]
         if 0<=nx<m and 0<=ny<n:
-            if data[nx][ny]:
-                if d+1<currdist[nx][ny]:
-                    currdist[nx][ny]=d+1
-                    heapq.heappush(q,[d+1,nx,ny])
-            else:
-                if d<currdist[nx][ny]:
-                    currdist[x][y]=d
-                    heapq.heappush(q,[d,nx,ny])
+            if data[nx][ny] and d+1<visited[nx][ny]:
+                visited[nx][ny]=d+1
+                heapq.heappush(q,[d+1,nx,ny])
+            elif not data[nx][ny] and d<visited[nx][ny]:
+                visited[nx][ny]=d
+                heapq.heappush(q,[d,nx,ny])
