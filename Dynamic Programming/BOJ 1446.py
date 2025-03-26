f5d7f9cdfa0ff0cccd017f58cca39a56e5c65a1a
acupoframen <choe.hwan@gmail.com>
2025-03-26 13:52:02 +0900

    BOJ 2025_03_26

---
+++ Dynamic Programming/BOJ 1446.py
@@ -0,0 +1,15 @@
+n,d=map(int,input().split())
+dp=list(i for i in range(d+1))
+dp[0]=0
+data=list(list(map(int,input().split())) for _ in range(n))
+data.sort(key=lambda x:(x[0],x[1],-x[2]))
+for j in range(n):
+    start,end,leng=data[j]
+    if end>d:
+        continue
+    if end-start<leng:
+        continue
+    dp[end]=min(dp[end],dp[start]+leng)
+    for i in range(1,d+1):
+        dp[i]=min(dp[i-1]+1,dp[i])
+print(dp[d])
